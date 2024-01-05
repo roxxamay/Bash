0f001157850f6827dd095aae7efdeacaa4d93ad6
roxxamay <itsmeadityakumararya@gmail.com>
2024-01-05 15:25:52 +0530

    variable done

---
+++ 02_Comments.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Checking Comments"
+
+#This is single line Comment
+
+
+<<comment
+This
+is
+multi
+line Comments
+comment
+
---
+++ 03_Var.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#script to show how to use variables
+
+a=10
+name=$(hostname)
+age=28
+
+echo "my name is $name and age is $age"
+
+name="aaddi"
+
+echo "my name is $name"
+
